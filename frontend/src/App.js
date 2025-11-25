aa79213119858c3ab864427a3ab47dd6ddf55bcd
Agaba-derrick <agabaderrick18@gmail.com>
2025-11-26 00:22:40 +0300

    simple fhir project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PatientSearch from "./PatientSearch";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>FHIR Patient Search</h2>
+      <PatientSearch />
+    </div>
+  );
+}
+
+export default App;
